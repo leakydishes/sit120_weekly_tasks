17505ca794217b5cfd4085413905a820af3c76e6
leakydishes <teclairestudios@gmail.com>
2022-07-22 12:56:29 +1000

    fixed folder name and file construction

---
+++ week_1/todo.js
@@ -0,0 +1,9 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { title: 'View Bricks Page', checked: true },
+            { title: 'Complete Contact Page', checked: false },
+        ],
+    },
+});
