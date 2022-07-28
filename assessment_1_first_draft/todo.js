903cf3ecb802c97db2776f06afe07e3410e8463e
leakydishes <teclairestudios@gmail.com>
2022-07-29 00:04:06 +1000

    Added Assessment1_first_version

---
+++ assessment_1_first_draft/todo.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { title: 'Name', checked: false },
+            { title: 'Email', checked: false },
+            { title: 'State', checked: false },
+        ],
+    },
+});
