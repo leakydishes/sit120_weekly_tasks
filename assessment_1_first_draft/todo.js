81473d3f53c1bbfb856be2cfca6dc319ee37ad27
leakydishes <teclairestudios@gmail.com>
2022-07-29 00:19:20 +1000

    Updated file structure

--- assessment_1_first_draft/todo.js
+++ assessment_1_first_draft/todo.js
@@ -2,9 +2,9 @@ let vue = new Vue({
     el: '#app',
     data: {
         todos: [
-            { title: 'Name', checked: false },
-            { title: 'Email', checked: false },
-            { title: 'State', checked: false },
+            { title: 'Yoga', checked: false },
+            { title: 'Assessment', checked: false },
+            { title: 'Work', checked: false },
         ],
     },
 });
