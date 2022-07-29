f29eddbc00af72fad20c59d3bd30d2396807f450
leakydishes <teclairestudios@gmail.com>
2022-07-29 21:22:58 +1000

    Assessment1 Folders

---
+++ assessment_1_first_draft/main.js
@@ -0,0 +1,26 @@
+//create a new vue application
+new Vue({
+
+    //connects vue to id todo in index.html
+    el: "#todo",
+
+    //all data from index is passed into here
+    data: {
+        //empty string 
+        newTask: "",
+        //empty array taskList
+        taskList: []
+    },
+
+    methods: {
+
+        //Coding Concepts:
+        //adds a task to a function
+
+        //remove a task listed by clicking a bin icon
+
+        //select all tasks to be passed into a variable to be checked
+
+        //store tasks in variables assigned to lists 
+        },
+});
